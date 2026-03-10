6ca58cd094da71870ec90c206c97678ecfac865e
Jeff <jmpedersen@my365.bellevue.edu>
2026-03-09 21:46:55 -0500

    added bg on timeline year header

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -93,10 +93,10 @@ const Timeline = ({ items, mobile = false }) => {
             {/* YEAR HEADER */}
             <div
               className={`
-                px-2 py-1 font-semibold text-slate-900 dark:text-slate-100
+                px-3 py-2 font-semibold text-slate-900 dark:text-slate-100
                 ${mobile
-                  ? "border-b border-slate-300 dark:border-slate-700 bg-slate-100 dark:bg-slate-900"
-                  : "sticky top-0 bg-slate-100/70 dark:bg-slate-900/70 backdrop-blur border-b border-slate-300 dark:border-slate-700"
+                  ? "border-b border-slate-300 dark:border-slate-700 bg-slate-100/70 dark:bg-slate-900/70 backdrop-blur shadow-sm"
+                  : "sticky top-0 bg-slate-100/70 dark:bg-slate-900/70 backdrop-blur-md border-b border-slate-300/70 dark:border-slate-700/70 rounded-md shadow-sm"
                 }
               `}
             >
