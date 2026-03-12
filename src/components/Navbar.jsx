97923698de12e5687611ea04f34f7b9880d1c3f8
Jeff <jmpedersen@my365.bellevue.edu>
2026-03-11 21:43:51 -0500

    added border changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,6 +24,7 @@ const navLinkClasses = ({ isActive }) =>
         : `
           group rounded-2xl p-4 flex flex-col gap-3 transition
           hover:bg-slate-50 
+          border:transparent
           hover:border
           hover:border-brand-soft 
           hover:shadow-[0_4px_20px_rgba(99,102,241,0.15)]
