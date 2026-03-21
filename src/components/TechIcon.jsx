4e9e59a07571c85be892ba771831a01bad8f43b4
Jeff <jmpedersen@my365.bellevue.edu>
2026-03-20 23:54:48 -0500

    updated skills icons bg

--- src/components/TechIcon.jsx
+++ src/components/TechIcon.jsx
@@ -71,22 +71,22 @@ const NAME_TO_ICON = {
 };
 
 const BG_COLORS = [
-  "bg-red-500",
-  "bg-orange-500",
-  "bg-amber-500",
-  "bg-yellow-500",
-  "bg-lime-500",
+  "bg-red-500/50",
+  "bg-orange-500/50",
+  "bg-amber-500/50",
+  "bg-yellow-500/50",
+  "bg-lime-500/50",
   "bg-green-500",
-  "bg-emerald-500",
-  "bg-teal-500",
-  "bg-cyan-500",
-  "bg-sky-500",
-  "bg-blue-500",
-  "bg-indigo-500",
-  "bg-violet-500",
-  "bg-purple-500",
-  "bg-fuchsia-500",
-  "bg-pink-500",
+  "bg-emerald-500/50",
+  "bg-teal-500/50",
+  "bg-cyan-500/50",
+  "bg-sky-500/50",
+  "bg-blue-500/50",
+  "bg-indigo-500/50",
+  "bg-violet-500/50",
+  "bg-purple-500/50",
+  "bg-fuchsia-500/50",
+  "bg-pink-500/50",
 ];
 
 const getColorFromString = (str) => {
