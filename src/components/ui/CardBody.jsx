9b22a477126d58837260abc303fa36dbdae9b2f9
Jeff <jmpedersen@my365.bellevue.edu>
2026-03-25 19:38:02 -0500

    updated lightmode card BG

--- src/components/ui/CardBody.jsx
+++ src/components/ui/CardBody.jsx
@@ -1,6 +1,6 @@
 const CardBody = ({ children }) => {
   return (
-    <div className="px-4 py-3 flex-1 bg-slate-200 dark:bg-slate-900">
+    <div className="px-4 py-3 flex-1 bg-slate-100 dark:bg-slate-900">
       {children}
     </div>
   );
