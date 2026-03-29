a5c0cf1111d1fc5d404cfec257df2bf5cff78911
Jeff <jmpedersen@my365.bellevue.edu>
2026-03-29 07:10:34 -0500

    updated lightmode BG on Home page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     <div
       className="
         min-h-screen flex flex-col
-        bg-gradient-to-br from-brand/5 via-transparent to-brand/10 text-slate-900
+        bg-slate-300 text-slate-900
         dark:bg-slate-950 dark:text-slate-100
         transition-colors
       "
