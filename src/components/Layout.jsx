fc3c348ccb88b25244372115e27b0ebd4f095364
Jeff <jmpedersen@my365.bellevue.edu>
2026-03-29 07:20:50 -0500

    updated lightmode BG on Home page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     <div
       className="
         min-h-screen flex flex-col
-        bg-indigo-300 text-slate-900
+        bg-indigo-200 text-slate-900
         dark:bg-slate-950 dark:text-slate-100
         transition-colors
       "
