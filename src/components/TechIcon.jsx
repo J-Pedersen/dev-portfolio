40bdc2ab64f1f2957ab76e9b03753d1ad5becc27
Jeff <jmpedersen@my365.bellevue.edu>
2026-04-26 08:52:20 -0500

    Addeded pm icon

--- src/components/TechIcon.jsx
+++ src/components/TechIcon.jsx
@@ -86,7 +86,7 @@ const NAME_TO_ICON = {
   "Stakeholder Management": "stakeholder_management-icon",
   Agile: "agile-icon",
   Scrum: "scrum-icon",
-  "Risk Register": "risk_register-icon",
+  "Risk Registers": "risk_register-icon",
 };
 
 const NAME_TO_BG = {
@@ -169,7 +169,7 @@ const NAME_TO_BG = {
   "Stakeholder Management": "bg-yellow-300/20 border border-brand dark:border-brand-soft",
   Agile: "bg-green-300/20 border border-brand dark:border-brand-soft",
   Scrum: "bg-blue-300/20 border border-brand dark:border-brand-soft",
-  "Risk Register": "bg-red-300/20 border border-brand dark:border-brand-soft",
+  "Risk Registers": "bg-red-300/20 border border-brand dark:border-brand-soft",
 };
 
 const RANDOM_BG_COLORS = [
