58bebfeb2e4b6285073f711d79903247bd2383d7
Jeff <jmpedersen@my365.bellevue.edu>
2026-04-26 04:38:24 -0500

    updated skills.jsx

--- src/components/TechIcon.jsx
+++ src/components/TechIcon.jsx
@@ -80,6 +80,7 @@ const NAME_TO_ICON = {
   "Project Scheduling": "project_scheduling-icon",
   "Resource Allocation": "resource_allocation-icon",
   "Cost Estimation": "cost_estimation-icon",
+  "Project Charter": "project_charter-icon",
 };
 
 const NAME_TO_BG = {
@@ -156,6 +157,7 @@ const NAME_TO_BG = {
   "Project Scheduling": "bg-neutral-300/20 border border-brand dark:border-brand-soft",
   "Resource Allocation": "bg-amber-300/20 border border-brand dark:border-brand-soft",
   "Cost Estimation": "bg-fuchsia-300/20 border border-brand dark:border-brand-soft",
+  "Project Charter": "bg-slate-300/20 border border-brand dark:border-brand-soft",
 };
 
 const RANDOM_BG_COLORS = [
