0ddc4d31b87ae1ee74c1de1e5bd9b8c7228cbfd9
Jeff <jmpedersen@my365.bellevue.edu>
2026-05-04 00:45:41 -0500

    updated modal close button

--- src/components/TechInfoModal.jsx
+++ src/components/TechInfoModal.jsx
@@ -83,7 +83,7 @@ const TechInfoModal = ({ open, onClose, tech, description, usedIn = [] }) => {
           <button
             type="button"
             className="
-              absolute top-2 right-3 text-white
+              absolute top-3 right-3 text-white
               h-8 w-8 rounded-full
               border border-white/20
               bg-white/10
