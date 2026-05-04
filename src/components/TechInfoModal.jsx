18e9d5fa2e69e6817992f17615e6a71592ff7e7c
Jeff <jmpedersen@my365.bellevue.edu>
2026-05-04 10:43:32 -0500

    updated TechIcon.jsx to remove BG transperancy for better contrast

--- src/components/TechInfoModal.jsx
+++ src/components/TechInfoModal.jsx
@@ -162,7 +162,7 @@ const TechInfoModal = ({ open, onClose, tech, description, usedIn = [] }) => {
                               <TechIcon
                                 name={icon}
                                 hideLabel
-                                showBg={true}
+                                showBg={false}
                                 interactive={false}
                                 className="scale-90 group-hover:scale-105 transition"
                               />
