ecaf5f2490ae9093da829e18d05ba28ca848788d
Jeff <jmpedersen@my365.bellevue.edu>
2026-05-08 02:50:06 -0500

    updated TechIcon.jsx to fix icon title text

--- src/components/TechIcon.jsx
+++ src/components/TechIcon.jsx
@@ -403,7 +403,7 @@ const TechIcon = ({
         <span
           className="
             absolute left-1/2 bottom-0 z-10
-            max-w-[4rem]
+            max-w-[4.2rem]
             -translate-x-1/2 translate-y-1
 
             rounded-md px-1 py-[1px]
